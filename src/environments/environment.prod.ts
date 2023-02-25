390c544a865f7535e590dc3b15510ef184854364
rrs-sistema <rivaldo.roberto@outlook.com>
2023-02-25 18:27:54 -0300

    Add express

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://127.0.0.1:8080/reobote',
+  apiUrl: 'http://ec2-54-157-174-140.compute-1.amazonaws.com:8080/reobote',
 
-  tokenWhitelistedDomains: [new RegExp('127.0.0.1:8080')],
+  tokenWhitelistedDomains: [new RegExp('http://ec2-54-157-174-140.compute-1.amazonaws.com:8080')],
   tokenBlacklistedRoutes: [new RegExp('\/oauth\/token')]
 };
