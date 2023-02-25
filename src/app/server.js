c1155cbef7b5d5e8def50eb0ffeaca73baab4ebb
rrs-sistema <rivaldo.roberto@outlook.com>
2023-02-25 18:59:54 -0300

    remove ghpages

--- src/app/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.use(express.static(__dirname + '/dist/reobote-pdv-gerencia'));
-
-app.get('/*', function(req, res){
-    res.sendFile(__dirname +  '/dist/reobote-pdv-gerencia/index.html');
-});
-app.listen(process.env.PORT || 4200);
