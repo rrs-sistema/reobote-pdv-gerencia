d4ea367157893605ba38c7698659b59c2bcbb8f1
rrs-sistema <rivaldo.roberto@outlook.com>
2023-02-27 16:14:57 -0300

    Update package.json

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
+//Install express server
 const express = require('express');
+const path = require('path');
+
 const app = express();
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/reobote-pdv-gerencia'));
 
-app.use(express.static(__dirname + '/dist/reobote-pdv-gerencia'));
-
-app.get('/*', function(req, res){
-    res.sendFile(__dirname +  '/dist/reobote-pdv-gerencia/index.html');
-});
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/reobote-pdv-gerencia/'}),
+);
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 4200);
