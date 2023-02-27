9afd50454e817d710fc9a5ea0499148537382b40
rrs-sistema <rivaldo.roberto@outlook.com>
2023-02-27 11:22:12 -0300

    Ajuste package.json e Server.js

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
-const express = require('express');
-// iniciar express
+const express = require('express')
+const path = require('path')
 const app = express();
-// nome da pasta no dist que sera feito o build
-const appName = 'reobote-pdv-gerencia';
-// local onde build ira gerar os arquivos
-const outputPath = `${__dirname}/dist/${appName}`;
-// seta o diretorio de build para servir o conteudo Angular
-app.use(express.static(outputPath));
-// redirecionar qualquer requisicao para o index.html
-app.get('/*', (req, res) => {
-  res.sendFile(`${outputPath}/index.html`);
+
+const appName = 'reobote-pdv-gerencia'
+
+app.use(express.static(__dirname + `/dist/${appName}`))
+
+app.get('/*', function (req, res){
+      res.sendFile(path.join(__dirname + `/dist/${appName}/index.html`))
 });
-// ouvir a porta que o Heroku disponibilizar
-app.listen(process.env.PORT || 4200);
+
+app.listen(process.env.PORT || 4200)
