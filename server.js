bb33df1b1f7276e07aad7daa288dc8835b493619
rrs-sistema <rivaldo.roberto@outlook.com>
2023-02-27 15:59:03 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-let express = require('express');
-let app = express();
+const express = require('express');
+const app = express();
 
-app.use(express.static(__dirname+'/dist/reobote-pdv-gerencia'));
-app.get('/*', function (req, res) {
-  res.sendFile(__dirname + '/dist/reobote-pdv-gerencia/index.html');
+app.use(express.static(__dirname + '/dist/reobote-pdv-gerencia'));
+
+app.get('/*', function(req, res){
+    res.sendFile(__dirname +  '/dist/reobote-pdv-gerencia/index.html');
 });
-
 app.listen(process.env.PORT || 4200);
