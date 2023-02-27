76c33acbb1a6dbbdc43f99902d9c9af8721e36cf
rrs-sistema <rivaldo.roberto@outlook.com>
2023-02-27 11:07:29 -0300

    Alterações gerais

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'http://ec2-54-157-174-140.compute-1.amazonaws.com:8080/reobote',
+  //apiUrl: 'http://localhost:8080',
 
   tokenWhitelistedDomains: [new RegExp('http://ec2-54-157-174-140.compute-1.amazonaws.com:8080')],
   tokenBlacklistedRoutes: [new RegExp('\/oauth\/token')]
