13d0e3f0767cebb1fcc1f01c6f9035a644fb0d34
rrs-sistema <rivaldo.roberto@outlook.com>
2023-09-28 16:18:17 -0300

    Subindo geral pro heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://ec2-54-157-174-140.compute-1.amazonaws.com:8080/reobote',
+  apiUrl: 'http://ec2-52-55-56-8.compute-1.amazonaws.com:8080/reobote',
   //apiUrl: 'http://localhost:8080',
 
-  tokenWhitelistedDomains: [new RegExp('http://ec2-54-157-174-140.compute-1.amazonaws.com:8080')],
+  tokenWhitelistedDomains: [new RegExp('http://ec2-52-55-56-8.compute-1.amazonaws.com:8080/reobote')],
   tokenBlacklistedRoutes: [new RegExp('\/oauth\/token')]
 };
