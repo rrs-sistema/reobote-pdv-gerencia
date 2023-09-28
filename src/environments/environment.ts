108725d948b5c9795cd3338f512571dc7e5f2d71
rrs-sistema <rivaldo.roberto@outlook.com>
2023-09-28 15:10:51 -0300

    Usando servidor local

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  //apiUrl: 'http://127.0.0.1:8080',//Para local
-  apiUrl: 'http://ec2-54-157-174-140.compute-1.amazonaws.com:8080/reobote',//Para Produção
+  apiUrl: 'http://127.0.0.1:8080',//Para local
+  //apiUrl: 'http://ec2-54-157-174-140.compute-1.amazonaws.com:8080/reobote',//Para Produção
 
   tokenWhitelistedDomains: [new RegExp('http://ec2-54-157-174-140.compute-1.amazonaws.com:8080')],
   tokenBlacklistedRoutes: [new RegExp('\/oauth\/token')]
